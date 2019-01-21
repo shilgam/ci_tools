7bc6fb657e43ed5915a77682b4b117edc30db71b
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-21 14:15:24 +0300

    comment out redundand lines

--- bin/test.sh
+++ bin/test.sh
@@ -4,6 +4,8 @@ set -e
 # Wait for services to be started
 dockerize -wait http://web:8000 \
           -wait http://hub:4444 \
-          -timeout 5s
+          -wait http://chrome-debug:5555 \
+          -wait http://firefox:5555 \
+          -timeout 20s
 
 exec "$@"
