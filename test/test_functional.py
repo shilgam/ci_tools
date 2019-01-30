f4f08947105149debc102b1297697c3eabdade26
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-30 23:24:26 +0300

    Func tests: Creating to-do item

--- test/test_functional.py
+++ test/test_functional.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 import unittest
 
 
@@ -17,7 +19,32 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list(self):
         self.browser.get(self.url)
+
+        # page title and header
         self.assertEqual('To-Do list', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # user is invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # type into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        # when user hits enter, the page updates, and now entered item
+        # appears in the list table
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     "New to-do item did not appear in table"
+        # )
 
 
 if __name__ == '__main__':
