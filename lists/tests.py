acc2637bd8e4c8ec59ebe72f520b2f5ec87aa053
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-30 14:36:44 +0300

    Use Django’s test client + Test implementation instead of testing constants

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,8 @@
-from django.urls import resolve
-from django.template.loader import render_to_string
 from django.test import TestCase
-from django.http import HttpRequest
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
