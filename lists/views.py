1509920f718d96ff151da43256d02532238ff68e
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-31 10:59:15 +0300

    Adapt the view to deal with a POST request

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_page(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST['item_text'])
     return render(request, 'home.html')
