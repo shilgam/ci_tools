d5f9d6af8a55fab9c3ae02e4794fddc10dd73845
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-31 13:52:40 +0300

    Pass python vars to be rendered in the template

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    if request.method == 'POST':
-        return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text', ''),
+    })
