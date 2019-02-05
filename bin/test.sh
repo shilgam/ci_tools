d37b0cf255041752aacd9fc2a93779b1ee7924f4
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-02-05 13:35:25 +0300

    docker-compose.test.yml: Remove unneeded WEB service

--- bin/test.sh
+++ bin/test.sh
@@ -2,8 +2,7 @@
 set -e
 
 # Wait for services to be started
-dockerize -wait http://web:8000 \
-          -wait http://hub:4444 \
+dockerize -wait http://hub:4444 \
           -wait http://chrome-debug:5555 \
           -wait http://firefox:5555 \
           -timeout 20s
