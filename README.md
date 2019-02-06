dfae8c2a5063a99abb74cc1ebcaab65a09fd6d86
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-02-06 15:09:11 +0300

    Add Func test: Items are associated w/ different lists

--- README.md
+++ README.md
@@ -24,14 +24,24 @@
 
 1. Run the test suite
 
-    1. Functional tests:
-
-            $ docker-compose --project-name functests --file docker-compose.test.yml up --abort-on-container-exit
-        Note: To visually see what the browser is doing you will need to create connection to VNC Server `localhost:5900`
-
-    1. Unit tests:
+    1. Unit tests
 
             $ docker-compose run web python manage.py test lists
+
+    1. Functional tests
+
+        1. build test environment:
+
+                $ docker-compose --project-name functests --file docker-compose.test.yml build
+
+        1. run tests:
+
+                $ docker-compose --project-name functests --file docker-compose.test.yml run --rm testweb
+            Note: To visually see what the browser is doing you will need to create connection to VNC Server `localhost:5900`
+
+        1. clean up after tests:
+
+                $ docker-compose --project-name functests --file docker-compose.test.yml down
 
 ## Deploy to Heroku
 
@@ -43,7 +53,7 @@
 
 1. Navigate to the app’s directory and create a Heroku app:
 
-        $ heroku create        
+        $ heroku create
         Creating salty-fortress-4191... done, stack is cedar-14
         https://salty-fortress-4191.herokuapp.com/ | https://git.heroku.com/salty-fortress-4191.git
 
--- functional_tests/test.py
+++ functional_tests/test.py
@@ -20,7 +20,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
             command_executor=selenium_hub_url,
             desired_capabilities=DesiredCapabilities.CHROME)
 
-    def teamDown(self):
+    def tearDown(self):
         self.browser.quit()
 
     def wait_for_row_in_list_table(self, row_text):
@@ -36,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(self.live_server_url)
 
         # page title and header
@@ -73,3 +73,15 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # User wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique url for her.
         # User visits that URL - her to-do list is still there.
+
+    def test_multiple_users_can_start_lists_at_different_ruls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        # self.assertRegex(edith_list_url, '/lists/.=')
