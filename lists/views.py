35d7352a361b0ab417a06389ce15f86400acb8eb
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-02-08 00:38:53 +0300

    Do not show list items in the Home page

--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,7 @@ from lists.models import Item
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/only-list-in-the-world')
+        return redirect('/lists/only-list-in-the-world/')
     return render(request, 'home.html')
 
 
