f0d864bab7283aec07ae81f8321e7a3c2e24d0a7
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-02-07 17:11:40 +0300

    Add URL for adding a new item to an existing list via POST

--- lists/tests.py
+++ lists/tests.py
@@ -58,3 +58,17 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+
+class newListTest(TestCase):
+
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/lists/only-list-in-the-world')
