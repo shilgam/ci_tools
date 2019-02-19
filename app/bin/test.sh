b27d58e478e739c7015f0ce706738fac48feca42
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-02-19 21:09:10 +0300

    Chrome: 2 nodes

--- app/bin/test.sh
+++ app/bin/test.sh
@@ -2,10 +2,8 @@
 set -e
 
 # Wait for services to be started
-# dockerize -wait http://hub:4444 \
-#           -wait http://chrome-debug:5555 \
-#           -wait http://firefox:5555 \
 dockerize -wait http://chrome:5555 \
+          -wait http://chrome2:5555 \
           -timeout 20s
 
 exec "$@"
