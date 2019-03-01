42b38b257e4592deb865b69fc31e674a17546a89
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-03-01 14:33:07 +0300

    Add Funt test to check layout

--- app/functional_tests/test.py
+++ app/functional_tests/test.py
@@ -117,3 +117,16 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
